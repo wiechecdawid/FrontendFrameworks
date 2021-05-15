e964bc7a50d07ef997da141faa0d0a30ce7111cc
Dawid Wiechec <wiechec.dawid@gmail.com>
2021-05-15 19:01:50 +0200

    SideOptions

--- my-app/src/components/SideMenu/SideMenu.tsx
+++ my-app/src/components/SideMenu/SideMenu.tsx
@@ -1,7 +1,11 @@
 import { ProfileSummary } from './ProfileSummary/ProfileSummary'
+import { SideOption } from './SideOption/SideOption'
 
 export const SideMenu = () => (
     <div>
         <ProfileSummary />
+        <SideOption option='Publications' />
+        <SideOption option='Ecosystem' />
+        <SideOption option='Entities' />
     </div>
 )
---
+++ my-app/src/components/SideMenu/SideOption/SideOption.tsx
@@ -0,0 +1,34 @@
+import { FC } from 'react'
+import styled from 'styled-components'
+import publications from '../../../assets/icons/publications.png'
+import ecosystem from '../../../assets/icons/ecosystem.png'
+import entities from '../../../assets/icons/entities.png'
+
+interface IProps {
+    option: string
+}
+
+const OptionWrapper = styled.div`
+    display: flex;
+`
+
+
+export const SideOption: FC<IProps> = ( { option } ) => {
+    const icon = () => {
+        switch(option) {
+        case 'Publications':
+            return publications;
+        case 'Ecosystem':
+            return ecosystem;
+        default:
+            return entities;
+        }
+    }
+
+    return (
+        <OptionWrapper>
+            <img src={icon()} alt="option" />
+            <div>{option}</div>
+        </OptionWrapper>
+    )
+}
